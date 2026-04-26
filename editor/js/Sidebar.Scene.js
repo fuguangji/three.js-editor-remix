f9db6b12cd5eb3615de262cf4cbc0c9ad36b73ec
fuguangji <fu60413@gmail.com>
2026-04-26 08:56:50 +0800

    Update Sidebar.Scene.js

--- editor/js/Sidebar.Scene.js
+++ editor/js/Sidebar.Scene.js
@@ -344,7 +344,6 @@ function SidebarScene( editor ) {
 
 	container.add( fogTypeRow );
     const testRow = new UIRow();
-
     const testButton = new UIButton();
 testButton.setLabel('測試按鈕');
 
@@ -360,9 +359,8 @@ testButton.setLabel('測試按鈕');
         console.log('目前選到：', object.name);
 
     });
-
-testRow.add(testButton);
-container.add(testRow);
+    testRow.add(testButton);
+    container.add(testRow);
 	// fog color
 
 	const fogPropertiesRow = new UIRow();
@@ -603,7 +601,16 @@ container.add(testRow);
 		}
 
 	} );
+const debugBtn = document.createElement('button');
+debugBtn.innerText = 'DEBUG BUTTON';
+debugBtn.style.position = 'fixed';
+debugBtn.style.bottom = '10px';
+debugBtn.style.left = '10px';
+debugBtn.style.zIndex = '9999';
 
+debugBtn.onclick = () => console.log('clicked');
+
+document.body.appendChild(debugBtn);
 	return container;
 
 }
